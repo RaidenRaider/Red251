135d6b2a443d89b5085a2b60e44d9dc09c2f1ceb
RaidenRaider <mattyblog@hotmail.co.uk>
2016-03-04 18:35:24 +0000

    Route 2 Wild Encounters Tweaked
    Increased Level Curve for Hard (Blue) Version.

--- data/wildPokemonH/route2.asm
+++ data/wildPokemonH/route2.asm
@@ -1,16 +1,16 @@
 Route2Mons:
 ; Grass Mons
 	db $19
-	db 3,SPINARAK
-	db 3,LEDYBA
-	db 4,PIDGEY
-	db 4,RATTATA
-	db 5,PIDGEY
-	db 3,WEEDLE
-	db 3,CATERPIE
-	db 5,RATTATA
+	db 5,SPINARAK
+	db 5,LEDYBA
+	db 6,PIDGEY
+	db 6,RATTATA
+	db 7,PIDGEY
 	db 5,WEEDLE
 	db 5,CATERPIE
+	db 7,RATTATA
+	db 7,WEEDLE
+	db 7,CATERPIE
 
 ; Water Mons
 	db $00
