c662c2698ee2f5b840666dc01761351f704ef383
RaidenRaider <mattyblog@hotmail.co.uk>
2016-03-04 18:38:38 +0000

    Route 3 Wild Encounters Tweaked
    Increased Level Curve for Extreme (Green) Version.

--- data/wildPokemonE/route3.asm
+++ data/wildPokemonE/route3.asm
@@ -1,16 +1,16 @@
 Route3Mons:
 ; Grass Mons
 	db $14
-	db 6,YANMA
-	db 5,HOOTHOOT
-	db 7,NATU
-	db 6,HOPPIP
-	db 7,SANDSHREW
-	db 8,PIDGEY
-	db 8,SPEAROW
-	db 3,SUNKERN
-	db 5,JIGGLYPUFF
-	db 7,JIGGLYPUFF
+	db 13,YANMA
+	db 12,HOOTHOOT
+	db 14,NATU
+	db 13,HOPPIP
+	db 14,SANDSHREW
+	db 15,PIDGEY
+	db 15,SPEAROW
+	db 10,SUNKERN
+	db 12,JIGGLYPUFF
+	db 14,JIGGLYPUFF
 
 ; Water Mons
 	db $00
